b0c4cbbedffc8298eb3e1abb1d05b567cb46a4e2
RobertAlden <randydonst@gmail.com>
2022-12-29 00:55:53 -0600

    refactored again

--- ProjectEuler/PE0011/pe0011.fsx
+++ ProjectEuler/PE0011/pe0011.fsx
@@ -7,18 +7,9 @@ The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
 
 What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
 *)
-
-let path = "grid.txt"
-let data = System.IO.File.ReadAllLines path |> Array.map (fun x -> x.Split ' ' |> Array.map int64)
-
-let grid = Array2D.init 20 20 (fun x y -> data.[y].[x])
-
-
 let windowed2D w h (arr:int64 array2d) = [for x in [0..(Array2D.length2 arr-w)] do
-                                            for y in [0..(Array2D.length1 arr-h)] do
-                                                 yield arr[x..x+w-1,y..y+w-1]]
-
-
+                                              for y in [0..(Array2D.length1 arr-h)] do
+                                                  yield arr[x..x+w-1,y..y+w-1]]
 let transpose (arr: int64 array2d) =  Array2D.init (arr.GetLength 1) (arr.GetLength 0) (fun x y -> arr[y,x])
 let reverse (arr: int64 array2d) =  Array2D.init (arr.GetLength 0) (arr.GetLength 1) (fun x y -> arr[(arr.GetLength 0 - 1) - x,y])
 let maxRowProduct (r: int64 array2d) = [for i in [0..((r.GetLength 1)-1)] -> r[i, *] |> Array.reduce(*)]
@@ -26,11 +17,13 @@ let maxRowProduct (r: int64 array2d) = [for i in [0..((r.GetLength 1)-1)] -> r[i
 let getDiag (r: int64 array2d) = List.zip [0..((r.GetLength 0)-1)] [0..((r.GetLength 1)-1)] 
                                  |> List.map (fun (x,y) -> r[x,y]) 
                                  |> List.reduce(*)
-let maxProduct (arr: int64 array2d) = 
-    List.max [maxRowProduct arr;
-              arr |> transpose |> maxRowProduct; 
-              getDiag arr; 
-              arr |> reverse |> getDiag;]
+let maxProduct (arr: int64 array2d) = List.max [maxRowProduct arr;
+                                                arr |> transpose |> maxRowProduct; 
+                                                getDiag arr; 
+                                                arr |> reverse |> getDiag;]
 
+let path = "grid.txt"
+let data = System.IO.File.ReadAllLines path |> Array.map (fun x -> x.Split ' ' |> Array.map int64)
+let grid = Array2D.init 20 20 (fun x y -> data.[y].[x])
 let answer =  List.max [for window in (windowed2D 4 4 grid) -> maxProduct window]
 printfn $"Answer: {answer}"
